9c0fc78248c845a14d8268a21fbd13f4ed04221c
Alex <alexurban12@gmail.com>
2021-07-27 20:36:23 +0200

    Added basic css

--- scripts.js
+++ scripts.js
@@ -1,10 +1,16 @@
 function computerPlay() {
     let answer; let random;
-    random = Math.random();
-    console.log(random);
-    answer = Math.floor(random);
+    random = Math.floor(Math.random())*3;
+    
+    if (random < 1) {
+        answer = "rock";
+    } else if (random>=1 && random <2) {
+        answer = "paper";
+    } else if (random>=2 && random<3) {
+        answer = "scissors";
+    }
     return answer;
 }
 
 temp = computerPlay();
-alert(temp);
+console.log(temp);
