51430fa0fa11d8d31873aee1db4bbfda7b937159
Alex <alexurban12@gmail.com>
2021-07-27 19:55:48 +0200

    Created basic template

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function computerPlay() {
+    let answer; let random;
+    random = Math.random();
+    console.log(random);
+    answer = Math.floor(random);
+    return answer;
+}
+
+computerPlay();
